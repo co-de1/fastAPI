05212c31beb2ca30d8a477f0a677a1c0a23439a5
Jose <jmantonio2000@gmail.com>
2025-04-02 02:34:47 -0300

    subindo atualizações

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+
+from src.fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')  # endreço de acesso no website / -->> raíz do site
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)  # endreço de acesso no website / -->> raíz do site
 def read_root():
     return {'message': 'Ola, mundoo'}
